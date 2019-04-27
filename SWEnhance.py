2add468cecbae21f5bea8031fa1e6e68abaccb37
Cara Monical <Cara@Delia.local>
2019-04-27 11:53:19 -0600

    Less runes in reapp report

--- SWEnhance.py
+++ SWEnhance.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     airking = Summoner(os.path.expanduser('~/Desktop/Summoners War Exporter Files/Airking77-23182654.json'))
     airking.analyze_grinds()
     #airking.print_runes()
-    airking.analyze_reapps(20)
+    airking.analyze_reapps(10)
